5feb05f8e326484acc24e84aa6ad2e11da263f20
Alberto M <swoord.sla@gmail.com>
2022-08-17 00:06:29 -0500

    feat(game-engine-lib): Expose components in the public API surface of lib.

--- projects/game-engine-lib/src/public-api.ts
+++ projects/game-engine-lib/src/public-api.ts
@@ -2,6 +2,8 @@
  * Public API Surface of game-engine-lib
  */
 
-export * from './lib/game-engine-lib.service';
-export * from './lib/game-engine-lib.component';
-export * from './lib/game-engine-lib.module';
+export * from "./lib/game-engine-lib.module";
+
+export * from "./lib/services/game-engine-lib.service";
+
+export * from "./lib/components/board/board.component";
