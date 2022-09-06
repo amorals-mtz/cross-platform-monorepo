9d34baaf5f2ec7493fb96b60af0ba93c0ad3e3a2
Alberto M <swoord.sla@gmail.com>
2022-09-06 13:04:07 -0500

    feat(tetris): Add service to communicate between Electron and Angular.
    * Since we cannot directly access all of Electron’s APIs from the Angular app, we need to make use of Inter-Process Communication (IPC). It is a mechanism the operating system provides so that two different processes (i.e. from main process to browser process and vice versa) can communicate with each other.

--- projects/tetris/src/app/app.component.ts
+++ projects/tetris/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { ElectronService } from './core/services/electron.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
@@ -7,5 +9,17 @@ import { Component } from '@angular/core';
 export class AppComponent {
 
   title = 'ng-tetris';
+
+  constructor(
+    private electronService: ElectronService
+  ) {
+    if (electronService.isElectron) {
+      console.log("Run in Electron");
+      console.log("Electron ipcRenderer", this.electronService.ipcRenderer);
+      console.log("NodeJS childProcess", this.electronService.childProcess);
+    } else {
+      console.log("Run in browser");
+    }
+  }
 
 }
--- projects/tetris/src/app/app.module.ts
+++ projects/tetris/src/app/app.module.ts
@@ -1,17 +1,20 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { GameEngineLibModule } from '@game-engine-lib';
 
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   imports: [
     BrowserModule,
-    GameEngineLibModule
+    GameEngineLibModule,
+    CoreModule
   ],
   declarations: [ AppComponent ],
   providers: [],
-  bootstrap: [ AppComponent ]
+  bootstrap: [ AppComponent ],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
 })
 export class AppModule { }
