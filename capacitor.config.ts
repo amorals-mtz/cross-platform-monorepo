229be7f50a7ff140a86421abbc7188ef69c562d5
Alberto M <swoord.sla@gmail.com>
2022-09-06 16:29:50 -0500

    build: Initialize `Capacitor` configuration file.

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.tetris.app',
+  appName: 'Cross Platform App',
+  webDir: 'dist/tetris',
+  bundledWebRuntime: false
+};
+
+export default config;
